00a2a4615a335d3ee362adfbe1d64b273411c772
Jose Lehmkuhl <josevicentekoerich@gmail.com>
2018-11-26 20:17:11 -0200

    added authors

--- EstDados/src/estdados/EstDados.java
+++ EstDados/src/estdados/EstDados.java
@@ -1,16 +1,8 @@
 package estdados;
 /*
-Metodos necessários:
-busca simples por criterio
-busca combinada entre dois critérios
-adiciona dados através de carga de dados
-adiciona dados através de inserção manual
-remoção de dados
-exibir dados
-
-demais coisas a fazer (qm precisa de trello se tem comentários:
-escolher qual será o caráter de nossos dados (gostei da ideia de personagem de one piece, na real)
-o trabalho deve acompanhar uma documentação que explica o porque das nossas decisoes de projeto
+-----AUTORES-----
+17203197 - Jose Lehmkuhl
+17203434 - Bayard Martins
 */
 
 import Controladores.ControladorPrincipal;
